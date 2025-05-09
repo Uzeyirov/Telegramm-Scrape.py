e6cd07d7a22a901b9fbb70e25d417c708a86a23c
Fikret <eynullauzeyirov@gmail.com>
2025-05-09 13:37:31 +0400

    Create Telegram-mesaj.py

---
+++ Telegram-mesaj.py
@@ -0,0 +1,41 @@
+import requests
+from bs4 import BeautifulSoup
+from openpyxl import Workbook
+import time
+from telegram import Bot
+
+#Telegram-Bot
+BOT_TOKEN = "7961154905:AAE39dcHN3GqA-la8WSJlboHTJLSHTtaH_4"
+CHAT_ID = "5882097855"
+
+bot = Bot(token=BOT_TOKEN)
+
+#Define URL (Book)
+
+url="https://emlak.az/"
+time.sleep(5)
+
+# Request page
+headers = {"User-Agent": "Mozilla/5.0"}
+response = requests.get(url, headers=headers)
+soup = BeautifulSoup(response.text, "html.parser")
+
+# Scrape listing
+emlak=soup.find_all("div", class_="ticket-item")[0:2]
+
+
+for product in emlak:
+    
+    price_tag = product.find("div", class_="price-ticket")
+    attribute_tag = product.find("div", class_="description-ticket")
+
+
+    price=price_tag.get_text(strip=True) if price_tag else ""
+    attribute=attribute_tag.get_text(strip=True) if attribute_tag else ""
+
+#
+mesaj = f"""
+🏠  <b>{attribute}</b>
+💰 Qiymət: <i>{price}</i>
+"""
+bot.send_message(chat_id=CHAT_ID, text=mesaj, parse_mode="HTML")
